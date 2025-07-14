894759ebb52b1247f01cdad59f76eac527b43fab
Bot473Gaming <tungnguyenduong473@gmail.com>
2025-07-14 22:09:15 +0700

    modify condition class

--- src/modules/class/dto/condition-class.dto.ts
+++ src/modules/class/dto/condition-class.dto.ts
@@ -2,5 +2,5 @@ import { PartialType, PickType } from '@nestjs/swagger';
 import { Class } from '../entities/class.entity';
 
 export class ConditionClassDto extends PartialType(
-  PickType(Class, ['grade', 'subject', 'location', 'mode']),
+  PickType(Class, ['grade', 'subject', 'location', 'mode', 'tutor_id']),
 ) {}
