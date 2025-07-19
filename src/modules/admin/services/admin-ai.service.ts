3e0cbc5c00860b166e47fcf0e9a6fabc157c524c
Bot473Gaming <tungnguyenduong473@gmail.com>
2025-07-19 14:32:38 +0700

    modify subject

--- src/modules/admin/services/admin-ai.service.ts
+++ src/modules/admin/services/admin-ai.service.ts
@@ -200,7 +200,7 @@ export class AdminAiService {
   }
 
   private async createSingleClass(tutor: User): Promise<Class> {
-    const subject = fakerVI.helpers.arrayElement(['Toán', 'Vật lý', 'Hoá Học', 'Tiếng Anh']);
+    const subject = fakerVI.helpers.arrayElement(['Toán', 'Vật lý', 'Hoá học', 'Tiếng Anh']);
     const grade = fakerVI.helpers.arrayElement(['10', '11', '12']);
     const topic = `${subject} lớp ${grade}`;
     const aiDetails = await this.generateClassDetails(topic);
