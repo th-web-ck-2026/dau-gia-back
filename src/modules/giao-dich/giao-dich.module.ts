d8106d4e171ad371e7c9850b45bd8d1fbd1c11e1
Bot473 <tungnguyenduong473@gmail.com>
2026-06-08 00:57:24 +0700

    feat(giao-dich): module + đăng ký app.module

---
+++ src/modules/giao-dich/giao-dich.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { GiaoDichModel } from './models/giao-dich.model';
+import { GiaoDichController } from './controllers/giao-dich.controller';
+import { GiaoDichService } from './services/giao-dich.service';
+import { GiaoDichRepository } from './repositories/giao-dich.repository';
+import { UsersModule } from '../user/user.module';
+
+@Module({
+  imports: [SequelizeModule.forFeature([GiaoDichModel]), UsersModule],
+  controllers: [GiaoDichController],
+  providers: [GiaoDichService, GiaoDichRepository],
+  exports: [GiaoDichService, GiaoDichRepository],
+})
+export class GiaoDichModule {}
