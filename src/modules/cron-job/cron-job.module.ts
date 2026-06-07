17bc28dfc8fd7bbc7eb63803fdba056d4e55b530
Bot473 <tungnguyenduong473@gmail.com>
2026-06-08 01:04:18 +0700

    feat(giao-dich): cron quét quá hạn xác nhận 48h

--- src/modules/cron-job/cron-job.module.ts
+++ src/modules/cron-job/cron-job.module.ts
@@ -3,10 +3,11 @@ import { CronJobService } from './cron-job.service';
 import { TenderModule } from '../tender/tender.module';
 import { AuctionModule } from '../auction/auction.module';
 import { NotificationModule } from '../notification/notification.module';
+import { GiaoDichModule } from '../giao-dich/giao-dich.module';
 
 @Global()
 @Module({
-  imports: [TenderModule, AuctionModule, NotificationModule],
+  imports: [TenderModule, AuctionModule, NotificationModule, GiaoDichModule],
   controllers: [],
   providers: [CronJobService],
   exports: [CronJobService],
