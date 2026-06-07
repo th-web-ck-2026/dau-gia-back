2741d155fc7e4aa45cc303d5e0adb23377930b5c
Bot473 <tungnguyenduong473@gmail.com>
2026-06-08 01:41:58 +0700

    update

--- src/modules/user/dto/condition-user.dto.ts
+++ src/modules/user/dto/condition-user.dto.ts
@@ -1,6 +1,23 @@
 import { PartialType, PickType } from '@nestjs/swagger';
 import { User } from '../entities/user.entity';
+import { IsOptional } from 'class-validator';
 
 export class ConditionUserDto extends PartialType(
-  PickType(User, ['_id', 'email', 'phone', 'fullname', 'role', 'userRoles', 'userStatus', 'isVerified']),
-) {}
+  PickType(User, [
+    '_id',
+    'email',
+    'phone',
+    'fullname',
+    'role',
+    'userRoles',
+    'userStatus',
+    'isVerified',
+  ]),
+) {
+  @IsOptional()
+  email?: string;
+  @IsOptional()
+  phone?: string;
+  @IsOptional()
+  fullname?: string;
+}
